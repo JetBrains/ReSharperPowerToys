c41b65d1b819c0720f6c2e0fc6c302effaf98b57
Hadi Hariri <mail@hadihariri.com>
2010-09-08 17:17:57 +0200

    Updated to 5.1.1

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,3 +1,5 @@
-msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.0.0 /p:VsVersion="9.0" /t:Clean
-msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.0.0 /p:VsVersion="9.0" /t:Build
-msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.0.0 /p:VsVersion="10.0" /t:Build
+msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.1.0 /p:VsVersion="8.0" /t:Clean
+msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.1.0 /p:VsVersion="8.0" /t:PrepareBinaries
+msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.1.0 /p:VsVersion="8.0" /t:Build
+msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.1.0 /p:VsVersion="9.0" /t:Build
+msbuild Build.proj /p:ReSharperVersion2=%1 /p:ProductVersion=%1.1.0 /p:VsVersion="10.0" /t:Build
