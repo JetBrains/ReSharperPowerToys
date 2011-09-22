4c3d188ab0efccfedb11835bd3d83142ae5f74aa
Leonid.Shalupov <leonid.shalupov@jetbrains.com>
2011-09-22 17:58:40 +0400

    missing files

---
+++ Src/ExploreTypeInterface/TypeInterfaceToolWindowDescriptor.cs
@@ -0,0 +1,13 @@
+using JetBrains.UI.ToolWindowManagement;
+
+namespace JetBrains.ReSharper.PowerToys.ExploreTypeInterface
+{
+  [ToolWindowDescriptor(
+    Id = "TypeInterface",
+    Text = "Type Interface",
+    Guid = "2EBEA6DE-578A-4234-A782-54F4F09B61D5",
+    VisibilityPersistenceScope = ToolWindowVisibilityPersistenceScope.Solution,
+    Type = ToolWindowType.MultiInstance)]
+  public class TypeInterfaceToolWindowDescriptor : ToolWindowDescriptor
+  { }
+}
