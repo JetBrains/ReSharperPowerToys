fd49ef3e32915cb6188d00eae1519b58ca4b6a47
Dmitri Nesteruk <Dmitri Nesteruk@localhost>
2011-12-08 15:49:04 +0400

    fixed transactional issue in zencoding powertoy

--- Src/ZenCoding/ZenCodingAction.cs
+++ Src/ZenCoding/ZenCodingAction.cs
@@ -42,37 +42,36 @@ namespace JetBrains.ReSharper.PowerToys.ZenCoding
 
       using (commandProcessor.UsingCommand("ZenCoding"))
       {
-        documentTransactionManager.StartTransaction("ZenCoding");
-
-        string abbr;
-        var abbrRange = textControl.Selection.OneDocRangeWithCaret();
-        if (abbrRange.IsValid && abbrRange.Length > 0)
+        using (documentTransactionManager.CreateTransactionCookie(DefaultAction.Commit, "ZenCoding"))
         {
-          abbr = textControl.Document.GetText(abbrRange);
-        }
-        else
-        {
-          var coords = textControl.Caret.PositionValue.ToDocLineColumn();
-          int start;
-          var engine = GetEngine(solution);
-          var lineText = textControl.Document.GetLineText(coords.Line);
-          abbr = engine.FindAbbreviationInLine(lineText, (int)coords.Column, out start);
-          if (start == -1)
+          string abbr;
+          var abbrRange = textControl.Selection.OneDocRangeWithCaret();
+          if (abbrRange.IsValid && abbrRange.Length > 0)
           {
-            Win32Declarations.MessageBeep(MessageBeepType.Error);
-            return;
+            abbr = textControl.Document.GetText(abbrRange);
           }
-          abbrRange = TextRange
-            .FromLength(textControl.Caret.PositionValue.ToDocOffset(), -abbr.Length)
-            .Normalized();
+          else
+          {
+            var coords = textControl.Caret.PositionValue.ToDocLineColumn();
+            int start;
+            var engine = GetEngine(solution);
+            var lineText = textControl.Document.GetLineText(coords.Line);
+            abbr = engine.FindAbbreviationInLine(lineText, (int) coords.Column, out start);
+            if (start == -1)
+            {
+              Win32Declarations.MessageBeep(MessageBeepType.Error);
+              return;
+            }
+            abbrRange = TextRange
+              .FromLength(textControl.Caret.PositionValue.ToDocOffset(), -abbr.Length)
+              .Normalized();
+          }
+
+          int insertPoint;
+          var projectFile = textControl.GetProjectFile(solution);
+          var expanded = GetEngine(solution).ExpandAbbreviation(abbr, GetDocTypeForFile(projectFile), out insertPoint);
+          CheckAndIndent(solution, projectFile, textControl, abbrRange, expanded, insertPoint);
         }
-
-        int insertPoint;
-        var projectFile = textControl.GetProjectFile(solution);
-        var expanded = GetEngine(solution).ExpandAbbreviation(abbr, GetDocTypeForFile(projectFile), out insertPoint);
-        CheckAndIndent(solution, projectFile, textControl, abbrRange, expanded, insertPoint);
-
-        documentTransactionManager.CommitTransaction(null);
       }
     }
   }
