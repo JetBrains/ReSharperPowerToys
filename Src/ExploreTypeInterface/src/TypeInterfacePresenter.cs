5b07fb85d7191d37ba9d53908a565becfe11cfd4
Leonid.Shalupov <leonid.shalupov@jetbrains.com>
2012-03-17 19:43:36 +0400

    compilation

--- Src/ExploreTypeInterface/src/TypeInterfacePresenter.cs
+++ Src/ExploreTypeInterface/src/TypeInterfacePresenter.cs
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-using JetBrains.ReSharper.Features.Common.TreePsiBrowser;
+using JetBrains.ReSharper.Features.Shared.TreePsiBrowser;
 using JetBrains.ReSharper.Psi;
 
 namespace JetBrains.ReSharper.PowerToys.ExploreTypeInterface
