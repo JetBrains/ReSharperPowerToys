5e80302c5b0fcd9949c31cddfbe807687089c3fa
Vadim.Romansky <Vadim.Romansky@localhost>
2012-06-04 19:26:41 +0400

    GeneratedContextSearch

---
+++ Src/PsiPlugin/src/Navigation/GeneratedContextSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Application.DataContext;
+using JetBrains.ReSharper.Feature.Services.ContextNavigation.ContextSearches;
+using JetBrains.ReSharper.Psi;
+
+namespace JetBrains.ReSharper.PsiPlugin.Navigation
+{
+  [FeaturePart]
+  class GeneratedContextSearch : IContextSearch
+  {
+    public bool IsAvailable(IDataContext dataContext)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool IsApplicable(IDataContext dataContext)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
