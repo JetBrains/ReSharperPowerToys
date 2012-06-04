9226e52f282eb91b0e713cfc25f28a1a3d5d5846
Vadim.Romansky <Vadim.Romansky@localhost>
2012-06-04 19:07:40 +0400

    compilation

--- Src/PsiPlugin/src/Navigation/PsiNavigeFromHereProvider.cs
+++ Src/PsiPlugin/src/Navigation/PsiNavigeFromHereProvider.cs
@@ -5,9 +5,9 @@ using JetBrains.Application.DataContext;
 using JetBrains.ReSharper.Feature.Services;
 using JetBrains.ReSharper.Feature.Services.ContextNavigation;
 using JetBrains.ReSharper.Feature.Services.ContextNavigation.ContextSearches;
-using JetBrains.ReSharper.Feature.Services.Search.SearchRequests;
-using JetBrains.ReSharper.Psi;
-using JetBrains.Util;
+using JetBrains.ReSharper.Features.Common.Occurences.ExecutionHosting;
+using JetBrains.ReSharper.Features.Finding.ExecutionHosting;
+using JetBrains.ReSharper.Features.Finding.NavigateFromHere;
 
 namespace JetBrains.ReSharper.PsiPlugin.Navigation
 {
@@ -23,92 +23,26 @@ namespace JetBrains.ReSharper.PsiPlugin.Navigation
 
     public IEnumerable<ContextNavigation> CreateWorkflow(IDataContext dataContext)
     {
-      //var execution = GetSearchesExecution(dataContext, DefaultNavigationExecutionHost.Instance);
-      /*if (execution != null)
+      var execution = GetSearchesExecution(dataContext, DefaultNavigationExecutionHost.Instance);
+      if (execution != null)
       {
         yield return new ContextNavigation(
           "Go to generated",
           null,
           NavigationActionGroup.Blessed,
           execution);
-      }*/
-      return null;
+      }
     }
 
-    /*public virtual Action GetSearchesExecution(IDataContext dataContext, INavigationExecutionHost host)
+    public virtual Action GetSearchesExecution(IDataContext dataContext, INavigationExecutionHost host)
     {
       var searches = ContextNavigationLanguageUtil.GetAvailableContextSearches<IDeclarationContextSearch>(dataContext, myManager);
       if (Enumerable.Any(searches))
         return () => Execute(dataContext, searches, host);
       return null;
-    }*/
-
-    /*protected void Execute(IDataContext dataContext, IEnumerable<IDeclarationContextSearch> searches,
-                                    INavigationExecutionHost host)
-    {
-      var elementsSearchPairs = searches.Select(x => x.GetElementsAndSearchRequest(dataContext));
-
-      //we assume that if in elements-request pair there are no elements then it's a special search request
-      var specialSearchRequest = TryFindSingleSpecialSearch(elementsSearchPairs);
-      if (specialSearchRequest != null)
-      {
-        //ExecuteSearchRequest(dataContext, specialSearchRequest(dataContext, null), host);
-        return;
-      }
-
-      var declaredElementsSearches = elementsSearchPairs.SelectMany(x => x.A.Select(declaredElement => JetTuple.Of(declaredElement, x.B)));
-      //ExecuteElementSearchPairs(dataContext, declaredElementsSearches, host);
-    }*/
-
-    /*protected void ExecuteSearchRequest(IDataContext context, [CanBeNull] SearchRequest searchRequest, INavigationExecutionHost host)
-    {
-      //search request can be null means the process of creating a searchRequest was interrupted by user.
-      if (searchRequest == null)
-        return;
-
-      string featureID = GetFeatureID();
-      if (!featureID.IsEmpty())
-      {
-        TipsManager.Instance.FeatureIsUsed(featureID, context);
-      }
-
-      var results = searchRequest.Search();
-      if (results == null)
-        return;
-      if (results.IsEmpty())
-      {
-        ShowResultMessage(context, GetNotFoundMessage(searchRequest));
-        return;
-      }
-
-      Func<TDescriptor> descriptorBuilder = () => CreateSearchDescriptor(searchRequest, results);
-
-      if (ProcessImmediateResult(context, results, host))
-        return;
-
-      ShowResults(context, host, searchRequest.Title, results, descriptorBuilder, Comparer.Create<IOccurence>(searchRequest.CompareOccurences));
-    }*/
-
-    protected GetSearchRequest<SearchDeclarationsRequest> TryFindSingleSpecialSearch(IEnumerable<JetTuple<IEnumerable<IDeclaredElement>, GetSearchRequest<SearchDeclarationsRequest>>> elementsSearchPairs)
-    {
-      foreach (JetTuple<IEnumerable<IDeclaredElement>, GetSearchRequest<SearchDeclarationsRequest>> elementSearchPair in elementsSearchPairs)
-      {
-        if (elementSearchPair.A.IsEmpty())
-        {
-          var request = elementSearchPair.B;
-          return request;
-        }
-      }
-
-      return null;
     }
 
-    private void Execute(IDataContext dataContext)
-    {
-      throw new NotImplementedException();
-    }
-
-    private static bool IsAvailable(IDataContext dataContext)
+    private void Execute(IDataContext dataContext, ICollection<IDeclarationContextSearch> searches, INavigationExecutionHost host)
     {
       throw new NotImplementedException();
     }
