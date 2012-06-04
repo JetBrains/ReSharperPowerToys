fdd25620825f2ccd4d827033debc7919ed755379
Vadim.Romansky <Vadim.Romansky@localhost>
2012-06-04 20:17:24 +0400

    goto generated class action

---
+++ Src/PsiPlugin/src/Navigation/GotoGeneratedClassAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.ActionManagement;
+using JetBrains.ReSharper.Features.Finding.GoToDeclaration;
+using JetBrains.ReSharper.Features.Finding.NavigateFromHere;
+
+namespace JetBrains.ReSharper.PsiPlugin.Navigation
+{
+  [ActionHandler(new string[] { "GotoGeneratedClass"})]
+  public class GotogeneratedClassAction : ContextNavigationActionBase<PsiNavigateFromHereProvider>
+  {
+  }
+}
