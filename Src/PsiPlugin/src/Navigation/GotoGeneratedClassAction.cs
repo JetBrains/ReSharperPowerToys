066f22c119552e42ca22280e8031831c103690c6
Vadim.Romansky <Vadim.Romansky@localhost>
2012-06-26 17:02:42 +0400

    compilation

--- Src/PsiPlugin/src/Navigation/GotoGeneratedClassAction.cs
+++ Src/PsiPlugin/src/Navigation/GotoGeneratedClassAction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using JetBrains.ActionManagement;
+using JetBrains.ReSharper.Features.Finding.NavigateFromHere;
 
 namespace JetBrains.ReSharper.PsiPlugin.Navigation
 {
