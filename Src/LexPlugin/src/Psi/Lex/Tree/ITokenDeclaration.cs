12a7a9d5775bfbab39333861b785cebf50034ce7
Vadim.Romansky <Vadim.Romansky@localhost>
2012-09-03 18:44:14 +0400

    lex token references

---
+++ Src/LexPlugin/src/Psi/Lex/Tree/ITokenDeclaration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace JetBrains.ReSharper.LexPlugin.Psi.Lex.Tree
+{
+  public partial interface ITokenDeclaration : IDeclaration
+  {
+  }
+}
---
+++ Src/LexPlugin/src/Psi/Lex/Tree/Impl/TokenTypeName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.ReSharper.LexPlugin.Resolve;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace JetBrains.ReSharper.LexPlugin.Psi.Lex.Tree.Impl
+{
+  internal partial class TokenTypeName
+  {
+    private LexTokenReference myTokenNameReference;
+
+    public LexTokenReference RuleNameReference
+    {
+      get
+      {
+        lock (this)
+          return myTokenNameReference ?? (myTokenNameReference = new LexTokenReference(this));
+      }
+    }
+
+    public override ReferenceCollection GetFirstClassReferences()
+    {
+      return new ReferenceCollection(RuleNameReference);
+    }
+
+    public void SetName(string shortName)
+    {
+      RuleNameReference.SetName(shortName);
+    }
+  }
+}
