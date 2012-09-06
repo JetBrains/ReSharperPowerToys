07bfd48ad1fc6c3102398f37fa7e5df30cb98022
Vadim.Romansky <Vadim.Romansky@localhost>
2012-09-06 13:12:50 +0400

    Lex completion

---
+++ Src/LexPlugin/src/Completion/LexCodeCompletionContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure;
+
+namespace JetBrains.ReSharper.LexPlugin.Completion
+{
+  class LexCodeCompletionContext : SpecificCodeCompletionContext
+  {
+    public LexCodeCompletionContext(CodeCompletionContext context, TextLookupRanges completionRanges, LexReparsedCompletionContext reparsedContext)
+      :
+        base(context)
+    {
+      ReparsedContext = reparsedContext;
+      Ranges = completionRanges;
+    }
+
+    public TextLookupRanges Ranges { get; private set; }
+
+    public LexReparsedCompletionContext ReparsedContext { get; private set; }
+
+    public override string ContextId
+    {
+      get { return "LexSpecificContext"; }
+    }
+  }
+}
