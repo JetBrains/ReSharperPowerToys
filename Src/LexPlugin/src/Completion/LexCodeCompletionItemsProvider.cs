4eeed299d2adce6688462fa051af0b41046a6377
Vadim.Romansky <Vadim.Romansky@localhost>
2012-09-06 13:12:50 +0400

    Lex completion

---
+++ Src/LexPlugin/src/Completion/LexCodeCompletionItemsProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Impl;
+using JetBrains.ReSharper.LexPlugin.Completion;
+using JetBrains.ReSharper.LexPlugin.Grammar;
+using JetBrains.ReSharper.LexPlugin.Psi.Lex.Tree.Impl;
+using JetBrains.ReSharper.LexPlugin.Resolve;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Resolve;
+
+namespace JetBrains.ReSharper.LexPlugin.src.Completion
+{
+  [Language(typeof(LexLanguage))]
+  internal class LexCodeCompletionItemsProvider : ItemsProviderWithReference<LexCodeCompletionContext, LexReferenceBase, LexFile>
+  {
+    protected override bool IsAvailable(LexCodeCompletionContext context)
+    {
+      if (!((context.BasicContext.CodeCompletionType == CodeCompletionType.BasicCompletion) || (context.BasicContext.CodeCompletionType == CodeCompletionType.AutomaticCompletion)))
+      {
+        return false;
+      }
+
+      IReference reference = context.ReparsedContext.Reference;
+      if (reference == null)
+      {
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
---
+++ Src/LexPlugin/src/Completion/LexIntellisenseManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Application.Settings;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Settings;
+
+namespace JetBrains.ReSharper.LexPlugin.Completion
+{
+  [SolutionComponent]
+  public class LexIntellisenseManager : LanguageSpecificIntellisenseManager
+  {
+    public LexIntellisenseManager(ISettingsStore settingsStore)
+      : base(settingsStore)
+    {
+      SettingsStore = settingsStore;
+    }
+
+    protected override bool GetIntellisenseEnabledSpecific(IContextBoundSettingsStore boundSettingsStore)
+    {
+      return boundSettingsStore.GetValue((IntellisenseEnabledSettingPsi setting) => setting.IntellisenseEnabled);
+    }
+  }
+
+  [SettingsKey(typeof (AutopopupEnabledSettingsKey), "Lex")]
+  public class LexAutopopupEnabledSettingsKey
+  {
+    [SettingsEntry(AutopopupType.HardAutopopup, "After dot")]
+    public AutopopupType AfterDot;
+
+    [SettingsEntry(AutopopupType.HardAutopopup, "On letters and digits")]
+    public AutopopupType OnIdent;
+  }
+
+  [SettingsKey(typeof (IntellisenseEnabledSettingsKey), "Override VS IntelliSense for Lex")]
+  public class IntellisenseEnabledSettingPsi
+  {
+    [SettingsEntry(false, "Lex (.lex files and embedded Lex)")]
+    public bool IntellisenseEnabled;
+  }
+}
