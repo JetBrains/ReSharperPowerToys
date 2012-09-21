d38b88ca2cd90ac5cc5430bf85d5c74da3e72e7c
Vadim.Romansky <Vadim.Romansky@localhost>
2012-09-21 20:45:18 +0400

    parameter info test

---
+++ Src/PsiPluginTest/src/Feature/Services/ParameterInfo/PsiParameterInfoTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.ReSharper.Feature.Services.ParameterInfo.Tests;
+using JetBrains.ReSharper.PsiPlugin.Grammar;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+
+namespace PsiPluginTest.Feature.Services.ParameterInfo
+{
+  [Category("PSI")]
+  [TestFileExtension(PsiProjectFileType.PsiExtension)]
+  public class PsiParameterInfoTest : ParameterInfoTestBase
+  {
+    protected override string RelativeTestDataPath
+    {
+      get { return @"feautere\services\parameterInfo"; }
+    }
+
+    [Test]
+    public void test001() { DoNamedTest(); }
+  }
+}
