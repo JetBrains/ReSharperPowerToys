f4bc68ad980ed7c8b5c9abae154bf31dc61c2e9a
Vadim.Romansky <Vadim.Romansky@localhost>
2012-09-24 19:07:21 +0400

    bug in generated fixed

--- Src/PsiPlugin/src/GeneratedDocument/Psi/CSharpFromPsiGenerator.cs
+++ Src/PsiPlugin/src/GeneratedDocument/Psi/CSharpFromPsiGenerator.cs
@@ -175,7 +175,7 @@ namespace JetBrains.ReSharper.PsiPlugin.GeneratedDocument.Psi
             return;
         }
       }
-      AddStaticClassOption(endOffset, startOffset, optionValueText, startInGenerated);
+      AddClassOption(endOffset, startOffset, optionValueText, startInGenerated);
     }
 
     private void AddClassOption(int endOffset, int startOffset, string optionValueText, int startInGenerated)
