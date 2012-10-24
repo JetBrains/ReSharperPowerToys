80f31341c83da2cd0d9a3539486a5831315d4340
Vadim.Romansky <Vadim.Romansky@localhost>
2012-10-24 20:10:03 +0400

    bug with formatter

--- Src/PsiPlugin/src/Formatter/PsiFormattingStage.cs
+++ Src/PsiPlugin/src/Formatter/PsiFormattingStage.cs
@@ -45,97 +45,12 @@ namespace JetBrains.ReSharper.PsiPlugin.Formatter
 
       var stage = new PsiFormattingStage(context);
 
-      IEnumerable<FormattingRange> nodePairs = GetNodePairs(context);
+      IEnumerable<FormattingRange> nodePairs = context.GetNodePairs();
 
       IEnumerable<FormatResult<IEnumerable<string>>> spaces = nodePairs.Select(
         range => new FormatResult<IEnumerable<string>>(range, stage.CalcSpaces(new FormattingStageContext(range))));
 
       FormatterImplHelper.ForeachResult(spaces, pi, res => stage.MakeFormat(res.Range, res.ResultValue));
-    }
-
-    private static IEnumerable<FormattingRange> GetNodePairs(CodeFormattingContext context)
-    {
-      ITreeNode firstNode = context.FirstNode;
-      ITreeNode lastNode = context.LastNode;
-      IList<FormattingRange> list = new List<FormattingRange>();
-      GetNodePairs(firstNode, lastNode, list);
-      return list;
-    }
-
-    private static void GetNodePairs(ITreeNode firstNode, ITreeNode lastNode, IList<FormattingRange> list)
-    {
-      var firstChild = firstNode;
-      var lastChild = lastNode;
-      var commonParent = firstNode.FindCommonParent(lastNode);
-      while (firstChild != null && firstChild.Parent != commonParent)
-      {
-        firstChild = firstChild.Parent;
-      }
-      while (lastChild != null && lastChild.Parent != commonParent)
-      {
-        lastChild = lastChild.Parent;
-      }
-      Assertion.Assert(firstChild != null, "firstChild != null");
-      Assertion.Assert(lastChild != null, "lastChild != null");
-      var node = firstChild;
-      while (node != null && node != lastChild.NextSibling)
-      {
-        if (!node.IsWhitespaceToken())
-        {
-          GetNodePairs(node, list, commonParent);
-        }
-        node = node.NextSibling;
-      }
-    }
-
-    private static void GetNodePairs(ITreeNode treeNode, IList<FormattingRange> list, ITreeNode parent)
-    {
-      ITreeNode node = treeNode;
-      if (node.FirstChild == null)
-      {
-        if (! node.IsWhitespaceToken())
-        {
-          ITreeNode nextNode = null;
-          while ((node != null)&&(nextNode == null))
-          {
-            var sibling = node.NextSibling;
-            while (sibling != null && sibling.IsWhitespaceToken())
-            {
-              sibling = sibling.NextSibling;
-            }
-            if (sibling == null)
-            {
-              node = node.Parent;
-              if (node == parent)
-              {
-                break;
-              }
-            }
-            else
-            {
-              nextNode = sibling;
-            }
-          }
-          if (nextNode != null)
-          {
-            list.Add(new FormattingRange(node, nextNode));
-          }
-        }
-      }
-      else
-      {
-        ITreeNode child = node.FirstChild;
-        {
-          while (child != null)
-          {
-            if (! child.IsWhitespaceToken())
-            {
-              GetNodePairs(child, list, parent);
-            }
-            child = child.NextSibling;
-          }
-        }
-      }
     }
 
     private IEnumerable<string> CalcSpaces(FormattingStageContext context)
