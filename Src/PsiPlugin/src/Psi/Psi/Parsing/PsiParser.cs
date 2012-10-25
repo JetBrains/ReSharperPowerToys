6e48b3a7dbd5846db4d632ba1d14ecdbb24df48d
Vadim.Romansky <Vadim.Romansky@localhost>
2012-10-25 18:19:57 +0400

    handling errors in rule body parsing

--- Src/PsiPlugin/src/Psi/Psi/Parsing/PsiParser.cs
+++ Src/PsiPlugin/src/Psi/Psi/Parsing/PsiParser.cs
@@ -1,10 +1,13 @@
-﻿using JetBrains.Application;
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Application;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
 using JetBrains.ReSharper.Psi.Parsing;
 using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.ReSharper.PsiPlugin.Lexer.Psi;
 using JetBrains.ReSharper.PsiPlugin.Psi.Psi.Tree.Impl;
+using JetBrains.Util;
 
 namespace JetBrains.ReSharper.PsiPlugin.Psi.Psi.Parsing
 {
@@ -60,6 +63,28 @@ namespace JetBrains.ReSharper.PsiPlugin.Psi.Psi.Parsing
     public override TreeElement parsePsiFile()
     {
       return ParsePsiFile(true);
+    }
+
+    protected override CompositeElement handleErrorInRuleBody(CompositeElement result, SyntaxError error)
+    {
+      TokenNodeType tokenType;
+      CompositeElement errorElement;
+      if (result is IErrorElement)
+      {
+        errorElement = result;
+      }
+      else
+      {
+        errorElement = TreeElementFactory.CreateErrorElement(error.Message);
+        result.AppendNewChild(errorElement);
+      }
+      tokenType = myLexer.TokenType;
+      while (tokenType != null && tokenType != TokenType.SEMICOLON && !IsInFollow(tokenType) && (! PsiTokenType.KEYWORDS.Contains(tokenType)))
+      {
+        skip(errorElement);
+        tokenType = myLexer.TokenType;
+      }
+      return result;     
     }
 
     public TreeElement ParsePsiFile(bool isFileReal)
